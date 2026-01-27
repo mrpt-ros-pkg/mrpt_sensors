10cfdade6350f380653cbe09621531c392efa075
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-01-27 01:28:50 +0100

    Fix obsolete tf headers

--- mrpt_sensor_gnss_novatel/src/mrpt_sensor_gnss_novatel_node.cpp
+++ mrpt_sensor_gnss_novatel/src/mrpt_sensor_gnss_novatel_node.cpp
@@ -44,8 +44,6 @@
 #include <mrpt/ros2bridge/gps.h>
 #include <mrpt/ros2bridge/time.h>
 #include <mrpt/system/COutputLogger.h>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
 
 #include <cmath>
 #include <mutex>
@@ -54,6 +52,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/detail/nav_sat_fix__struct.hpp>
 #include <sensor_msgs/msg/imu.hpp>
+#include <tf2/LinearMath/Matrix3x3.hpp>
+#include <tf2/LinearMath/Quaternion.hpp>
 
 #include "mrpt_sensorlib/mrpt_sensorlib.h"
 
